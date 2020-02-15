798c712b6911454f41f0e5c83dbb04f79e68834f
knuu <premier3next@gmail.com>
2020-02-16 01:10:44 +0900

    ignore boost for oj-helper

--- c++-library/boost.cpp
+++ c++-library/boost.cpp
@@ -1,9 +1,11 @@
-#include <boost/numeric/ublas/matrix.hpp>
-#include <boost/numeric/ublas/io.hpp>
+#define IGNORE
+
 #include <boost/multiprecision/cpp_int.hpp>
+#include <boost/numeric/ublas/io.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
 
 using mpi = boost::multiprecision::int128_t;
-//using mpi = boost::multiprecision::cpp_int // 任意桁
+// using mpi = boost::multiprecision::cpp_int // 任意桁
 
 using namespace boost::numeric;
 using imatrix = ublas::matrix<mpi>;
