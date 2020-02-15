470ea4eba7611e7e4457ad640720d123deba33ef
knuu <premier3next@gmail.com>
2020-02-16 03:12:33 +0900

    fix python test for tree diameter

--- tests/tree_diameter.test.py
+++ tests/tree_diameter.test.py
@@ -1,11 +1,18 @@
 # verify-helper: PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_5_A
+# @import python-library/graph/graph.py
 # @import python-library/graph/tree_diameter.py
+import sys
+
+sys.path.insert(0, ".")
+
+from python_library.graph.graph import Graph
+from python_library.graph.tree_diameter import TreeDiameter
 
 
 def aoj():
     N = int(input())
     graph = Graph(N)
-    for _ in range(N-1):
+    for _ in range(N - 1):
         s, t, w = map(int, input().split())
         graph.add_edge(s, t, w)
         graph.add_edge(t, s, w)
