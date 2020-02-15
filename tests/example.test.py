9fa7ed692d298b01980d383935d40bad05626d5f
knuu <premier3next@gmail.com>
2020-02-16 02:33:11 +0900

    add python example

---
+++ tests/example.test.py
@@ -0,0 +1,10 @@
+# verify-helper: PROBLEM https://judge.yosupo.jp/problem/aplusb
+
+
+def main() -> None:
+    A, B = map(int, input().split())
+    print(A + B)
+
+
+if __name__ == '__main__':
+    main()
